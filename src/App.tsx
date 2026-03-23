7334f16ddec27b1890ee513a584bd65e170645ac
=ben <benbest987@gmail.com>
2026-03-23 19:04:17 +1300

    feat: added css for Win95 styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+
 const App = () => {
-  return <div>EPL Tracker</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
